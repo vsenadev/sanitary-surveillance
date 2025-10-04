97b36beee8cc54ba3e44b2a4d07c191503bbd1ba
vsenadev <senavinicius01@gmail.com>
2025-10-04 20:37:08 -0300

    FEAT - storage updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
+#!/bin/sh
 set -e
 
+# Start IRIS in the background
 iris start IRIS quietly
 
+# Run your import script every container start
 iris session IRIS < /opt/irisapp/iris.script >> /opt/irisapp/logs/entrypoint.log 2>&1
 
+# Stop background if needed (optional, for clean start)
 iris stop IRIS quietly
 
+# Hand over control to the default IRIS entrypoint
 exec /iris-main "$@"
